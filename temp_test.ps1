786b3b055a437a8f4da4ce4aee1c1d60d765a10e
VEXO-RE <vventas@vexorealestate.com>
2026-04-30 01:15:27 -0600

    Clean repo with updated .gitignore

--- temp_test.ps1
+++
@@ -1,2 +0,0 @@
-$idx = "dummy"
-if ($idx -match '<script[^>]+src=[^>]*src/data/data\.js') { Write-Host ok } else { Write-Host fail }
--- temp_test2.ps1
+++
@@ -1,5 +0,0 @@
-$idx = "dummy"
-if ($idx -match '<script[^>]+src=[^>]*src/data/data\.js') { Write-Host "Script ok" } else { Write-Host "No script" }
-if ($idx -match 'FUENTE UNICA:\s*src/data/data\.js') { Write-Host "Coment ok" } else { Write-Host "No coment" }
-if ($idx -match 'design-system\.css') { Write-Host "CSS ok" } else { Write-Host "No css" }
-if ($idx -match '</di\s+<div') { Write-Host "Broken" } else { Write-Host "No broken" }
